497e60a878d8347942a0b8970b3fd5d3c70d56bf
18657 <18657@18657.com>
2020-12-30 23:37:45 +0800

    初始化了Spring Boot OAuth Login Example，但跳转后无法登录

---
+++ SpringBoot-OAuth-Login-Example/src/main/java/org/example/oauth/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package org.example.oauth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login.html").permitAll()
+                .anyRequest().authenticated();
+
+        http.oauth2Login().loginPage("/login.html").defaultSuccessUrl("/success.html");
+    }
+}
---
+++ SpringBoot-OAuth-Login-Example/src/main/java/org/example/oauth/controller/UserController.java
@@ -0,0 +1,19 @@
+package org.example.oauth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Principal> user(Principal principal) {
+        return ResponseEntity.ok(principal);
+    }
+
+}
