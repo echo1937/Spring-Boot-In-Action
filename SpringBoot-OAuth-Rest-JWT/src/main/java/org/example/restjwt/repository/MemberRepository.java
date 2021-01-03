9e244be897e8bd1f50fc87b3afe5d82d41df7d63
18657 <18657@18657.com>
2021-01-03 17:45:36 +0800

    因为kotlin的问题，该模块进展缓慢

---
+++ SpringBoot-OAuth-Rest-JWT/src/main/java/org/example/restjwt/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package org.example.restjwt.repository;
+
+
+import org.example.restjwt.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
